bd5b62a932b94cd4b0cc7682382333ac15d86234
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-03-29 22:53:37 -0500

    Update TV.java

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,70 @@
+package taller3.televisores;
+
+public class TV {
+    private Marca marca;
+    private boolean estado;
+    private int canal = 1;
+    private int volumen = 1;
+    private int precio = 500;
+    private static int numTV = 0;
+
+    public Marca getMarca() {
+        return this.marca;
+    }
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public Control getControl() {
+        return this.control;
+    }
+    public void setControl(Control control) {
+        this.control = control;
+    }
+
+    public int getPrecio() {
+        return this.precio;
+    }
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getVolumen() {
+        return this.volumen;
+    }
+    public void setVolumen(int volumen) {
+        if (!this.estado) {return;}
+        if (volumen < 0 || volumen > 7) {
+            return;
+        }
+        this.volumen = volumen;
+    }
+
+    public int getCanal() {
+        return this.canal;
+    }
+    public void setCanal(int canal) {
+        if (!this.estado) {return;}
+        if (canal < 1 || canal > 120) {
+            return;
+        }
+        this.canal = canal;
+    }
+
+    public static int getNumTV() {
+        return TV.numTV;
+    }
+
+    public void turnOn() {
+        this.estado = true;
+    }
+    public void turnOff() {
+        this.estado = false;
+    }
+
+    public boolean getEstado() {
+        return this.estado;
+    }
+
+
+}
